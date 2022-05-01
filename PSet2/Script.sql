1e8e00a3c7e00fd0863e21c1d9062ab86af03ca0
phsiller <91988694+phsiller@users.noreply.github.com>
2022-04-30 21:13:05 -0300

    Delete Script.sql

--- PSet2/Script.sql
+++
@@ -1,5 +0,0 @@
-/* Questão 01 */
-SELECT nome_departamento AS Departamento, CONCAT('R$', CAST(AVG(salario) AS DECIMAL(10,2))) AS Média_salarial
-FROM funcionarios f INNER JOIN departamento d
-WHERE f.numero_departamento = d.numero_departamento
-GROUP BY nome_departamento;
