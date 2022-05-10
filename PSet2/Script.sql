d58e3d26e7367bd3d8738aaeb1a2e9aa5e885c2a
phsiller <91988694+phsiller@users.noreply.github.com>
2022-05-10 17:20:11 -0300

    Questão 5

--- PSet2/Script.sql
+++ PSet2/Script.sql
@@ -23,3 +23,11 @@ WHERE salario < '35000' UNION
 SELECT CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo, FLOOR(DATEDIFF(CURDATE(), data_nascimento)/365.25) AS Idade, 
 CONCAT('R$ ', CAST((salario) AS DECIMAL(10,2))) AS Salário, CONCAT('R$ ', CAST((salario*1.15) AS DECIMAL(10,2))) AS Novo_salário FROM funcionarios f
 WHERE salario >= '35000';
+
+
+/* Questão 05 */
+SELECT nome_departamento AS Departamento, g.primeiro_nome AS Gerente, f.primeiro_nome AS Funcionário, 
+CONCAT('R$ ', CAST((salario) AS DECIMAL(10,2))) AS Salário FROM departamento d INNER JOIN funcionarios f, 
+(SELECT primeiro_nome, cpf FROM funcionarios f INNER JOIN departamento d WHERE f.cpf = d.cpf_gerente) AS g
+WHERE d.numero_departamento = f.numero_departamento AND g.cpf = d.cpf_gerente
+ORDER BY d.nome_departamento ASC, f.salario DESC;
