0817310ad349b89aa579115c0648460670106004
phsiller <91988694+phsiller@users.noreply.github.com>
2022-05-12 18:56:14 -0300

    Questão 14

--- PSet2/Script.sql
+++ PSet2/Script.sql
@@ -95,6 +95,9 @@ WHEN sexo = 'F' THEN 'Feminino' WHEN sexo = 'f' THEN 'Feminino' END AS Sexo,
 FLOOR(DATEDIFF(CURDATE(), d.data_nascimento)/365.25) AS Idade FROM dependente d ORDER BY Idade;
 
 
+/* QUESTÃO 14 */
+SELECT d.nome_departamento AS Departamento, COUNT(f.numero_departamento) AS Número_funcionários FROM funcionarios f INNER JOIN departamento d
+WHERE f.numero_departamento = d.numero_departamento GROUP BY d.nome_departamento;
 
 
 
