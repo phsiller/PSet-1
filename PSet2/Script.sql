15a79f7df1f41b220784f3405edb15eefbf72799
phsiller <91988694+phsiller@users.noreply.github.com>
2022-05-12 19:51:38 -0300

    Questão 15

--- PSet2/Script.sql
+++ PSet2/Script.sql
@@ -100,5 +100,14 @@ SELECT d.nome_departamento AS Departamento, COUNT(f.numero_departamento) AS Núm
 WHERE f.numero_departamento = d.numero_departamento GROUP BY d.nome_departamento;
 
 
-
+/* Questão 15 */
+SELECT DISTINCT CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo,
+d.nome_departamento AS Departamento, 
+p.nome_projeto AS Projeto FROM departamento d INNER JOIN projeto p INNER JOIN trabalha_em t INNER JOIN funcionarios f 
+WHERE d.numero_departamento = f.numero_departamento AND p.numero_projeto = t.numero_projeto AND t.cpf_funcionario = f.cpf
+UNION
+SELECT DISTINCT CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo,
+d.nome_departamento AS Departamento, 
+'Sem projeto' AS Projeto FROM departamento d INNER JOIN projeto p INNER JOIN trabalha_em t INNER JOIN funcionarios f 
+WHERE d.numero_departamento = f.numero_departamento AND p.numero_projeto = t.numero_projeto AND (f.cpf NOT IN (SELECT t.cpf_funcionario FROM trabalha_em t));
 
